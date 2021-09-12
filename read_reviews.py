271be543cfc28c96530badcb6e6f5aa5ec659a3b
CKC <chinkuanchen@gmail.com>
2021-09-12 12:18:56 +0800

    calculate aveage length

--- read_reviews.py
+++ read_reviews.py
@@ -1,12 +1,14 @@
 data = []
 count = 0
+length_acc = 0
 with open('reviews.txt','r') as f:
     for line in f:
+        length = len(line)
+        length_acc += length
         data.append(line)
         count += 1
         if count % 10000 == 0:
             print(len(data))
-print(len(data))
-print(data[0])
-print('--------------------')
-print(data[1])
+print('檔案讀取完了，總共有', len(data), '筆資料')
+length_ave = length_acc / len(data)
+print('每筆資料平均長度：', length_ave, '字')
